4bd4782018192b60feca6cc5d31ebfefe3e4570c
Joe Natalzia <jnatalzia@gmail.com>
2014-03-06 09:25:57 -0500

    Finalized and fixed hand class Now includes code for drawing both hands, and has access to both hand positions

--- bigfoot/classes/Hand.js
+++ bigfoot/classes/Hand.js
@@ -1,62 +1,173 @@
 window.Hand = function()
 {
+	this.size = {width:130,height:130};
+	this.x = 0;
+	this.y = 0;
+
+	this.spriteSheet = new Image();
+	this.spriteSheet.src = "img/hand_sprite.png";
+
+	this.handX = 0,this.handTwoX=0;
+	this.handY = 0,this.handTwoY=0;
 
 	this.draw = function(frame,ctx)
 	{
-			var hand = frame.hands[0];
-			var numFingers;
-		if(frame.hands[0])
+		var hand = frame.hands[0];
+		var hand2 = frame.hands[1];
+		var numFingers,sx,sy;
+		var numFingers2,sx2,sy2;
+		if(frame.hands.length == 1)
 		{
 			var fingers = hand.fingers.length;
 			switch(fingers)
 			{
 				case 0:
-					numFingers = 0;
-					Hand.x = hand.palmPosition[0];
-					Hand.y = hand.palmPosition[1];
-					this.handPosition(Hand.x,Hand.y,numFingers);
+					sx = 130;
+					sy=260;
 					break;
 				case 1:
-					numFingers = 1;
-					var f1 = hand.fingers[0];
-					f1.x = f1.tipPosition[0];
-					f1.y = f1.tipPosition[1];
-					this.handPosition(f1.x,f1.y,numFingers);
+					sx = 260;
+					sy=260;
 					break;
 				case 2:
-					numFingers = 2;
-					Hand.x = hand.palmPosition[0];
-					Hand.y = hand.palmPosition[1];
-					this.handPosition(Hand.x,Hand.y,numFingers);
+					sx = 390;
+					sy=260;
 					break;
 				case 3:
-					numFingers = 3;
-					Hand.x = hand.palmPosition[0];
-					Hand.y = hand.palmPosition[1];
-					this.handPosition(Hand.x,Hand.y,numFingers);
+					sx = 520;
+					sy=260;
 					break;
 				case 4:
-					numFingers = 4;
-					Hand.x = hand.palmPosition[0];
-					Hand.y = hand.palmPosition[1];
-					this.handPosition(Hand.x,Hand.y,numFingers);			
+					sx = 650;
+					sy=260;		
 					break;
 				case 5:
-					numFingers = 5;
-					Hand.x = hand.palmPosition[0];
-					Hand.y = hand.palmPosition[1];
-					this.handPosition(Hand.x,Hand.y,numFingers);
+					sx = 650;
+					sy=130;
 					break;
 			}
 			//console.log(numFingers);
 			//console.log(handPosition);
+			if (fingers != 1)
+			{
+				Hand.x = hand.palmPosition[0];
+				Hand.y = hand.palmPosition[1];
+			}
+			else
+			{
+				Hand.x = hand.fingers[0].tipPosition[0];
+				Hand.y = hand.fingers[0].tipPosition[1];
+			}
+			this.handX = map(Hand.x,-150,150,0,browserWidth);
+			this.handY = map(Hand.y,100,300,browserHeight,0);
+
+			ctx.globalAlpha = 0.5;
+			ctx.drawImage(this.spriteSheet,sx,sy,this.size.width,this.size.height,this.handX - this.size.width/2,this.handY - this.size.height/2,this.size.width,this.size.height);
+			ctx.globalAlpha = 1.0;
 		}
+		else if (frame.hands.length == 2)
+		{
+			//find out which hand is more left
+			var palm1 = hand.palmPosition[0];
+			var palm2 = hand2.palmPosition[0];
+
+			var lefthand = (palm1 > palm2) ? hand2 : hand;
+			var righthand = (palm1 > palm2) ? hand : hand2;
+
+			var fingersOne = righthand.fingers.length;
+			var fingersTwo = lefthand.fingers.length;
+			switch(fingersOne)
+			{
+				case 0:
+					sx = 130;
+					sy=260;
+					break;
+				case 1:
+					sx = 260;
+					sy=260;
+					break;
+				case 2:
+					sx = 390;
+					sy=260;
+					break;
+				case 3:
+					sx = 520;
+					sy=260;
+					break;
+				case 4:
+					sx = 650;
+					sy=260;		
+					break;
+				case 5:
+					sx = 650;
+					sy=130;
+					break;
+			}
+			switch(fingersTwo)
+			{
+				case 0:
+					sx2 = 130;
+					sy2=780;
+					break;
+				case 1:
+					sx2 = 260;
+					sy2=780;
+					break;
+				case 2:
+					sx2 = 390;
+					sy2=780;
+					break;
+				case 3:
+					sx2 = 520;
+					sy2=780;
+					break;
+				case 4:
+					sx2 = 650;
+					sy2=780;		
+					break;
+				case 5:
+					sx2 = 650;
+					sy2 =650;
+					break;
+			}
+
+			if (fingersOne != 1)
+			{
+				Hand.x = righthand.palmPosition[0];
+				Hand.y = righthand.palmPosition[1];
+			}
+			else
+			{
+				Hand.x = righthand.fingers[0].tipPosition[0];
+				Hand.y = righthand.fingers[0].tipPosition[1];
+			}
+			this.handX = map(Hand.x,-150,150,0,browserWidth);
+			this.handY = map(Hand.y,100,300,browserHeight,0);
+
+			if (fingersTwo != 1)
+			{
+				Hand.x = lefthand.palmPosition[0];
+				Hand.y = lefthand.palmPosition[1];
+			}
+			else
+			{
+				Hand.x = lefthand.fingers[0].tipPosition[0];
+				Hand.y = lefthand.fingers[0].tipPosition[1];
+			}
+			this.handTwoX = map(Hand.x,-150,150,0,browserWidth);
+			this.handTwoY = map(Hand.y,100,300,browserHeight,0);
+
+			ctx.globalAlpha = 0.5;
+			ctx.drawImage(this.spriteSheet,sx,sy,this.size.width,this.size.height,this.handX - this.size.width/2,this.handY - this.size.height/2,this.size.width,this.size.height);
+			ctx.drawImage(this.spriteSheet,sx2,sy2,this.size.width,this.size.height,this.handTwoX- this.size.width/2,this.handTwoY- this.size.height/2,this.size.width,this.size.height);
+			ctx.globalAlpha = 1.0;
+		}
+
+		//draw current frame
 	}
-	this.handPosition = function(x,y,numFingers)
+	this.handPosition = function(x,y)
 	{
-		var handX = map(Hand.x,-150,150,0,browserWidth);
-		var handY = map(Hand.y,100,300,browserHeight,0);
-		console.log("Hand x = "+handX+", Hand y = "+handY+", Fingers = "+numFingers);
+		
 	}
 }
 
