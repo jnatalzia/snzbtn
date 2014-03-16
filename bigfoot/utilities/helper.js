9eb6eb754f173705fcce2f854dbf6da9355ca587
Joe Natalzia <jnatalzia@gmail.com>
2014-03-16 18:43:55 -0400

    Added helper methods for dealing with different screensize and height

---
+++ bigfoot/utilities/helper.js
@@ -0,0 +1,18 @@
+function getCorrectedPosition(pos)
+{
+	var newpos = {x:0,y:0};
+
+	var ratio = {width:window.innerWidth/1920,height:window.innerHeight/1080};
+
+	newpos.x = pos.x * ratio.width;
+	newpos.y = pos.y * ratio.height;
+}
+function getCorrectedSize(size)
+{
+	var newsize = {width:0,height:0};
+
+	var ratio = {width:window.innerWidth/1920,height:window.innerHeight/1080};
+
+	newpos.width = size.width * ratio.width;
+	newpos.height = size.height * ratio.height;
+}
