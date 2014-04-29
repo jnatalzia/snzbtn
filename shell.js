028ac0b8d70597a594d505b371bc70a20b1aeba7
dhc6855 <dhc6855@rit.edu>
2014-04-29 15:29:47 -0400

    List of images to load
    here is the whole list for the loader

---
+++ shell.js
@@ -0,0 +1,70 @@
+
+// the Modernizr object is from the modernizr.custom.js file
+Modernizr.load(
+	{ 
+		// load all of these files
+		load : [
+			// insert "filename" here,
+			"img/buildBar.png",
+			"img/emptyVariable.png",
+			"img/hand_sprite.png",
+			"img/runBTN_active.png",
+			"img/text-box.png",
+			"img/toolbox-btm.png",
+			"img/toolbox-top.png",
+			"img/toolbox.png",
+			"img/levelSelect/1.png",
+			"img/levelSelect/2.png",
+			"img/levelSelect/3.png",
+			"img/levelSelect/4.png",
+			"img/levelSelect/conditionals.png",
+			"img/levelSelect/functions.png",
+			"img/levelSelect/lessons.png",
+			"img/levelSelect/loops.png",
+			"img/levelSelect/title.png",
+			"img/levelSelect/variables.png",
+			"img/lvl1/ash.png",
+			"img/lvl1/bee.png",
+			"img/lvl1/bg-dark.png",
+			"img/lvl1/bg-light.png",
+			"img/lvl1/blank_floor.png",
+			"img/lvl1/box1-empty.png",
+			"img/lvl1/box2-empty.png",
+			"img/lvl1/box3-empty.png",
+			"img/lvl1/glass_LV1.png",
+			"img/lvl1/glass_LV2.png",
+			"img/lvl1/glass_LV3.png",
+			"img/lvl1/glassVariable.png",
+			"img/lvl1/grab_hand.png",
+			"img/lvl1/metal_LV1.png",
+			"img/lvl1/metal_LV2.png",
+			"img/lvl1/metal_LV3.png",
+			"img/lvl1/metalVariable.png",
+			"img/lvl1/success.png",
+			"img/lvl1/whale_fly.png",
+			"img/lvl1/whale_happy.png",
+			"img/lvl1/whale_old.png",
+			"img/lvl1/whale_sad.png",
+			"img/lvl1/whale_talking.png",
+			"img/lvl1/wood_LV1.png",
+			"img/lvl1/wood_LV2.png",
+			"img/lvl1/wood_LV3.png",
+			"img/lvl1/woodVariable.png",
+			"img/lvl1/zombie.png",
+			"img/lvl1/floors/1-empty.png",
+			"img/lvl1/floors/2-empty.png",
+			"img/lvl1/floors/3-empty.png",
+		],
+		
+		
+		
+		// when the loading is complete, this function will be called
+		complete: function(){
+			
+			// set up event handlers
+
+			
+		} // end complete
+		
+	} // end object
+); // end Modernizr.load
